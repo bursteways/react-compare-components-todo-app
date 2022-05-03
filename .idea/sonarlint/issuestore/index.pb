3bf8e3561380298944cffe347607b410fc650172
Stephen Burke <Sburke0708@gmail.com>
2022-05-03 09:59:05 -0400

    Adds the ability to toggle checkboxes

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,10 +1,6 @@
 
->
-pages/_app.tsx,5/5/550528cc072fa7d76d123e2f4303d4ab39daa8e8
 B
 styles/globals.css,8/c/8c258232cbcd5d83991c524a9e86c8d3f984fc57
-=
-next-env.d.ts,7/9/7948598afd6f2aa542f1b55d7b6f077a70937e77
 :
 
 .gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
@@ -12,9 +8,27 @@ B
 .prettierrc.json,1/e/1ee5d199ddbeaaa1717e65b449ac7a6f03bed689
 C
 components/index.ts,0/5/05dbfbf07fc5a6a9b19ce700edff227ea53054e0
+m
+=components/TODO-via-functions/components/text-input/index.tsx,d/9/d9e0927db3cca1b8d6d1accada039ad09ce37508
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+k
+;components/TODO-via-classes/components/text-input/index.tsx,6/7/67f4f50cf71c46a925a81cf21e8c1fc0dcd1413d
+=
+tsconfig.json,6/1/61ebb9fd6e8cf9082658121d5d81e297791dacd0
+B
+tailwind.config.js,0/9/0918cbb76f2c44f223706cec9d31a32cd1177ce4
+A
+postcss.config.js,3/f/3f70684581fd16ab3be77ca959e0138ef3ee8bfb
+;
+.yarnrc.yml,9/d/9da6c3f8f0ef45beafba161d8dd084500ea7d884
+>
+pages/_app.tsx,5/5/550528cc072fa7d76d123e2f4303d4ab39daa8e8
 a
 1components/TODO-via-functions/components/index.ts,b/9/b96dd9625e9fe776cf0445725f2a316fbf57d73e
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
 ?
-pages/index.tsx,b/3/b3357635c26d1b52053382ce7e8a8640d31fedc8
-_
-/components/TODO-via-classes/components/index.ts,a/5/a5544b472cba5cf631fd22da119bc89cc5ddb60e
+.pnp.loader.mjs,7/a/7ad03ee4dee8d76a46a648e5949c805b5452d4bf
+>
+next.config.js,8/1/811a6d5a2e04fadb44d8eec365f2fa8af5c4facf
--- components/TODO-via-functions/index.tsx
+++ components/TODO-via-functions/index.tsx
@@ -1,14 +1,46 @@
-import { DOMElement, forwardRef, ReactElement, useRef, useState } from 'react';
+import { ReactElement, RefObject, useEffect, useRef, useState } from 'react';
 
 // Components
 import { TextInput } from './components';
 
+// Mocks
+import MOCK_ITEMS from 'static/mocks/items.json';
+
+// Utils
+import { mockFetch } from 'utils';
+
+// Types
+import { Todo } from 'types';
+
 export const TODOViaFunctions = (): ReactElement => {
-  const [todoState, setTodoState] = useState();
-  const inputEl = useRef<HTMLInputElement>(null);
-  const onButtonClick = () => {
+  const [todoItems, setTodoItems] = useState<Todo[]>([]);
+  const inputEl: RefObject<HTMLInputElement> = useRef<HTMLInputElement>(null);
+
+  const onButtonClick = (): void => {
+    // Focus the user's cursor back to the input after adding.
     inputEl.current?.focus();
   };
+
+  const onToggleComplete = (id: number): void => {
+    // Flip the state of the checked/unchecked item.
+    const updatedItems: Todo[] = todoItems.map(
+      (item: Todo): Todo =>
+        item.id === id
+          ? {
+              ...item,
+              isComplete: !item.isComplete,
+            }
+          : item,
+    );
+
+    // Set the state with the updated items.
+    setTodoItems(updatedItems);
+  };
+
+  useEffect(() => {
+    // Fetch from an `API` on load.
+    mockFetch().then((): void => setTodoItems(MOCK_ITEMS));
+  }, []);
 
   return (
     <div className="bg-secondary p-4">
@@ -29,6 +61,29 @@ export const TODOViaFunctions = (): ReactElement => {
           </button>
         </div>
       </form>
+
+      {/* List items */}
+      <ul>
+        {todoItems.map((item: Todo): ReactElement => {
+          const { id, isComplete, title } = item;
+
+          return (
+            <li className="flex my-4" key={id}>
+              <div className="form-control">
+                <label className="label cursor-pointer">
+                  <input
+                    checked={isComplete}
+                    className="checkbox mr-2"
+                    onChange={(): void => onToggleComplete(id)}
+                    type="checkbox"
+                  />
+                  <span className="label-text">{title}</span>
+                </label>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
